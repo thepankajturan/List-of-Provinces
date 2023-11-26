3323f07244a9205ebfdf0494b48f2e4bddaea354
Pankaj <thepankajturan@gmail.com>
2023-11-26 02:49:34 -0500

    Update paths for flags hrefs

--- scripts/panpanka.js
+++ scripts/panpanka.js
@@ -44,16 +44,16 @@ function startup() {
 
            const provincesData = JSON.parse(this.responseText);
 
-           ab.innerHTML = `<a href="/pages/second.html?name=${provincesData[0].name}"><img class="flag-img" src="${provincesData[0].flag}" alt="${provincesData[0].name}" /> <br> ${provincesData[0].name}</a>`;
-           bc.innerHTML = `<a href="/pages/second.html?name=${provincesData[1].name}"><img class="flag-img" src="${provincesData[1].flag}" alt="${provincesData[1].name}" /> <br> ${provincesData[1].name}</a>`;
-           mb.innerHTML = `<a href="/pages/second.html?name=${provincesData[2].name}"><img class="flag-img" src="${provincesData[2].flag}" alt="${provincesData[2].name}" /> <br> ${provincesData[2].name}</a>`;
-           nb.innerHTML = `<a href="/pages/second.html?name=${provincesData[3].name}"><img class="flag-img" src="${provincesData[3].flag}" alt="${provincesData[3].name}" /> <br> ${provincesData[3].name}</a>`;
-           nl.innerHTML = `<a href="/pages/second.html?name=${provincesData[4].name}"><img class="flag-img" src="${provincesData[4].flag}" alt="${provincesData[4].name}" /> <br> ${provincesData[4].name}</a>`;
-           ns.innerHTML = `<a href="/pages/second.html?name=${provincesData[5].name}"><img class="flag-img" src="${provincesData[5].flag}" alt="${provincesData[5].name}" /> <br> ${provincesData[5].name}</a>`;
-           on.innerHTML = `<a href="/pages/second.html?name=${provincesData[6].name}"><img class="flag-img" src="${provincesData[6].flag}" alt="${provincesData[6].name}" /> <br> ${provincesData[6].name}</a>`;
-           pe.innerHTML = `<a href="/pages/second.html?name=${provincesData[7].name}"><img class="flag-img" src="${provincesData[7].flag}" alt="${provincesData[7].name}" /> <br> ${provincesData[7].name}</a>`;
-           qc.innerHTML = `<a href="/pages/second.html?name=${provincesData[8].name}"><img class="flag-img" src="${provincesData[8].flag}" alt="${provincesData[8].name}" /> <br> ${provincesData[8].name}</a>`;
-           sk.innerHTML = `<a href="/pages/second.html?name=${provincesData[9].name}"><img class="flag-img" src="${provincesData[9].flag}" alt="${provincesData[9].name}" /> <br> ${provincesData[9].name}</a>`;
+           ab.innerHTML = `<a href="List-of-Provinces/pages/second.html?name=${provincesData[0].name}"><img class="flag-img" src="${provincesData[0].flag}" alt="${provincesData[0].name}" /> <br> ${provincesData[0].name}</a>`;
+           bc.innerHTML = `<a href="List-of-Provinces/pages/second.html?name=${provincesData[1].name}"><img class="flag-img" src="${provincesData[1].flag}" alt="${provincesData[1].name}" /> <br> ${provincesData[1].name}</a>`;
+           mb.innerHTML = `<a href="List-of-Provinces/pages/second.html?name=${provincesData[2].name}"><img class="flag-img" src="${provincesData[2].flag}" alt="${provincesData[2].name}" /> <br> ${provincesData[2].name}</a>`;
+           nb.innerHTML = `<a href="List-of-Provinces/pages/second.html?name=${provincesData[3].name}"><img class="flag-img" src="${provincesData[3].flag}" alt="${provincesData[3].name}" /> <br> ${provincesData[3].name}</a>`;
+           nl.innerHTML = `<a href="List-of-Provinces/pages/second.html?name=${provincesData[4].name}"><img class="flag-img" src="${provincesData[4].flag}" alt="${provincesData[4].name}" /> <br> ${provincesData[4].name}</a>`;
+           ns.innerHTML = `<a href="List-of-Provinces/pages/second.html?name=${provincesData[5].name}"><img class="flag-img" src="${provincesData[5].flag}" alt="${provincesData[5].name}" /> <br> ${provincesData[5].name}</a>`;
+           on.innerHTML = `<a href="List-of-Provinces/pages/second.html?name=${provincesData[6].name}"><img class="flag-img" src="${provincesData[6].flag}" alt="${provincesData[6].name}" /> <br> ${provincesData[6].name}</a>`;
+           pe.innerHTML = `<a href="List-of-Provinces/pages/second.html?name=${provincesData[7].name}"><img class="flag-img" src="${provincesData[7].flag}" alt="${provincesData[7].name}" /> <br> ${provincesData[7].name}</a>`;
+           qc.innerHTML = `<a href="List-of-Provinces/pages/second.html?name=${provincesData[8].name}"><img class="flag-img" src="${provincesData[8].flag}" alt="${provincesData[8].name}" /> <br> ${provincesData[8].name}</a>`;
+           sk.innerHTML = `<a href="List-of-Provinces/pages/second.html?name=${provincesData[9].name}"><img class="flag-img" src="${provincesData[9].flag}" alt="${provincesData[9].name}" /> <br> ${provincesData[9].name}</a>`;
             
         }
         
